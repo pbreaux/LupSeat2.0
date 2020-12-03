581d6686a632013635667736129b577e0304eaa2
HiDoYa <hiroyagojo@gmail.com>
2020-12-02 22:20:53 -0800

    Unit tests for chunk increase

--- tests/chunkincrease_test.py
+++ tests/chunkincrease_test.py
@@ -3,40 +3,40 @@ from models.assign import *
 from models.seat import *
 
 class ChunkIncreaseMethods(unittest.TestCase):
-    def test_distribute_empty(self):
+    def test_distribute_empty_2pass(self):
         sg = SeatGroups(('a', 4), ('a', 10))
-        _, chk_size_l = ChunkIncrease.get_empty_seats_inds(sg, 5)
-        empty_inds = ChunkIncrease.distribute_empty(sg, 5, chk_size_l)
+        _, chk_size_l = ChunkIncrease.distribute_empty(sg, 5)
+        empty_inds = ChunkIncrease.distribute_empty_2pass(sg, 5, chk_size_l)
         self.assertTrue(('a', 7) in empty_inds)
 
         sg = SeatGroups(('a', 4), ('a', 16))
-        _, chk_size_l = ChunkIncrease.get_empty_seats_inds(sg, 3)
-        empty_inds = ChunkIncrease.distribute_empty(sg, 3, chk_size_l)
+        _, chk_size_l = ChunkIncrease.distribute_empty(sg, 3)
+        empty_inds = ChunkIncrease.distribute_empty_2pass(sg, 3, chk_size_l)
         self.assertTrue(('a', 6) in empty_inds)
         self.assertTrue(('a', 9) in empty_inds)
         self.assertTrue(('a', 13) in empty_inds)
 
-    def test_get_empty_seats_inds(self):
+    def test_distribute_empty(self):
         sg = SeatGroups(('a', 4), ('a', 10))
-        empty_inds, chk_size_l = ChunkIncrease.get_empty_seats_inds(sg, 5)
+        empty_inds, chk_size_l = ChunkIncrease.distribute_empty(sg, 5)
         self.assertTrue(('a', 9) in empty_inds)
         self.assertEqual(chk_size_l, [5, 1])
 
         sg = SeatGroups(('a', 4), ('a', 10))
-        empty_inds, chk_size_l = ChunkIncrease.get_empty_seats_inds(sg, 2)
+        empty_inds, chk_size_l = ChunkIncrease.distribute_empty(sg, 2)
         self.assertTrue(('a', 6) in empty_inds)
         self.assertTrue(('a', 9) in empty_inds)
         self.assertEqual(chk_size_l, [2, 2, 1])
 
         sg = SeatGroups(('a', 4), ('a', 9))
-        empty_inds, chk_size_l = ChunkIncrease.get_empty_seats_inds(sg, 2)
+        empty_inds, chk_size_l = ChunkIncrease.distribute_empty(sg, 2)
         self.assertTrue(('a', 6) in empty_inds)
         self.assertTrue(('a', 8) in empty_inds)
         self.assertFalse(('a', 9) in empty_inds)
         self.assertEqual(chk_size_l, [2, 1, 1])
 
         sg = SeatGroups(('a', 4), ('a', 12))
-        empty_inds, chk_size_l = ChunkIncrease.get_empty_seats_inds(sg, 3)
+        empty_inds, chk_size_l = ChunkIncrease.distribute_empty(sg, 3)
         self.assertTrue(('a', 7) in empty_inds)
         self.assertTrue(('a', 11) in empty_inds)
         self.assertEqual(chk_size_l, [3, 3, 1])
